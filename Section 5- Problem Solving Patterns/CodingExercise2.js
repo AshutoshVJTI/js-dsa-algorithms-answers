733da34b2ce24db2cc40ab3ae417dfc6c4b405c0
Ashutosh <mathoreashutosh@gmail.com>
2024-09-28 14:00:08 -0700

    Format Questions

--- Section 5- Problem Solving Patterns/CodingExercise2.js
+++ Section 5- Problem Solving Patterns/CodingExercise2.js
@@ -1,5 +1,6 @@
 // Multiple Pointers - countUniqueValues
-// Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted.
+// Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. 
+// There can be negative numbers in the array, but it will always be sorted.
 
 // Time Complexity - O(n)
 
